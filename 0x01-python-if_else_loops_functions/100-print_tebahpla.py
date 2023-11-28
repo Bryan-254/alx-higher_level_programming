b7beb98aa225ff898deb9d45ebc561aaedf4ac67
UserBryan <bkk@gmail.com>
2023-11-28 08:05:53 -0800

    Program that prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,7 +3,7 @@ for x in range(122, 96, -1):
     # checks if the index is odd number
     if x % 2:
         # Difference btwn lowercase & uppercase letters is 32
-        # Subtracting 32 from x converts it to the ASCII code 
+        # Subtracting 32 from x converts it to the ASCII code
         # of the corresponding uppercase letter.
         x = x - 32
     print("{:c}".format(x), end="")
