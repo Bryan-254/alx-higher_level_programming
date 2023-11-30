4d33ec1500969a1db0077dbb8c552b9254963786
UserBryan <bkk@gmail.com>
2023-11-30 04:52:02 -0800

    ByteCode -> Python #3 task

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
-    add, sub = __import__('magic_calculation_102', globals(), locals(), ['add', 'sub'], 0)
+    add, sub = __import__('magic_calculation_102', globals(), locals(), ('add', 'sub'), 0)
 
     if a < b:
         c = add(a, b)
