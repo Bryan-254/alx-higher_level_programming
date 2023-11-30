4783856574db08b3771422cf5c32101c4233d7fc
UserBryan <bkk@gmail.com>
2023-11-30 02:02:21 -0800

    Import a simple function from a simple file task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 
-def add(a, b):
-    return a + b
-
+from add_0 import add
 a = 1
 b = 2
-
-from add_0 import add
-
 result = add(a, b)
 print("{} + {} = {}".format(a, b, result))
