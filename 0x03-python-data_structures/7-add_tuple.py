4feb2060cfbe5b04c5ff6ccedcc2ae6be0b3c168
UserBryan <bkk@gmail.com>
2023-12-02 06:45:17 -0800

    Function that adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Ensure each tuple has at least 2 elements, 
+    # Ensure each tuple has at least 2 elements,
     # filling with 0 if needed
     a = tuple_a + (0, 0)[:max(0, 2 - len(tuple_a))]
     b = tuple_b + (0, 0)[:max(0, 2 - len(tuple_b))]
