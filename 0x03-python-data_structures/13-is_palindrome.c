b3f86e73c3081f78df648011381ab19212e60afa
UserBryan <bkk@gmail.com>
2023-12-04 02:52:15 -0800

    Linked list palindrome task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,62 +1,44 @@
-#include "lists.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "lists.h"
 /**
- * add_nodeint - This adds a new node at the beginning of a listint_t list
- * @head: This is the head of listint_t
- * @n: The integer to add in listint_t list
- * Return: The address of the new element, or NULL if fail
+ * palindrome_rec - Iterates through a list recursively
+ * @head: head of the list
+ * @tail: iterates to the end of the list
+ *
+ * Return: 1 if a palindrome, 0 if not a palindrome
  */
 
-listint_t *add_nodeint(listint_t **head, const int n)
+int palindrome_rec(listint_t **head, listint_t *tail)
 {
-	listint_t *new_node;
+	if (tail == NULL)
+		return (1);
 
-	new_node = malloc(sizeof(listint_t));
-
-	if (!new_node)
+	if (palindrome_rec(head, tail->next) == 1 && (*head)->n == tail->n)
 	{
-		return (NULL);
+		(*head) = (*head)->next;
+		return (1);
 	}
 
-	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+	else
+		return (0);
 }
 
+
 /**
- * is_palindrome - This identifies if a syngle linked list is palindrome
- * @head: This is the head of listint_t
- * Return: 1 if it is palindrome otherwise zero
+ * is_palindrome - checks if linked list is a palindrome
+ * @head: head of the list
+ *
+ * Return: 1 if a palindrome, 0 if not a palindrome
  */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *head2 = *head;
-	listint_t *temp1 = NULL, *temp2 = NULL;
-
-	if (!*head || head2->next == NULL)
-	{
+	if (*head == NULL)
 		return (1);
-	}
-	while (head2 != NULL)
-	{
-		add_nodeint(&temp1, head2->n);
-		head2 = head2->next;
-	}
-	temp2 = temp1;
-	while (*head != NULL)
-	{
-		if ((*head)->n != temp2->n)
-		{
-			free_listint(temp1);
-			return (0);
-		}
-		*head = (*head)->next;
-		temp2 = temp2->next;
-	}
-	free_listint(temp1);
-	return (1);
+
+	if ((*head)->next == NULL)
+		return (1);
+
+	return (palindrome_rec(head, *head));
 }
