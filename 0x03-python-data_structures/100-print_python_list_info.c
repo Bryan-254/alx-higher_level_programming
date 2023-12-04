d4e7b8e43fca8537b0f12e2024514da8bbed8a0f
UserBryan <bkk@gmail.com>
2023-12-04 05:06:40 -0800

    CPython #0: Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,7 +7,7 @@
  * @p: This parameter is the python list
  */
 
-void print_python_list_info(PyObject *p) 
+void print_python_list_info(PyObject *p)
 {
 	PyListObject *list = (PyListObject *)p;
 
@@ -17,6 +17,7 @@ void print_python_list_info(PyObject *p)
 	for (Py_ssize_t i = 0; i < PyList_Size(p); ++i)
 	{
 		PyObject *item = PyList_GetItem(p, i);
+
 		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
 	}
 }
