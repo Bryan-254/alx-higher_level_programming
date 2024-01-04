39722bea878e428d10ab73707d32af59675d5f45
UserBryan <bkk@gmail.com>
2024-01-04 04:24:47 -0800

    Write a class Rectangle that defines a rectangle by: (based on 2-rectangle.py)

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-Write a class Rectangle that defines a rectangle
+Write an empty class Rectangle that defines a rectangle
 """
+
 
 class Rectangle:
     """creates class that defines a rectangle
