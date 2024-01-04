a0483e3bff6faa751677915d57a1a7991269e039
UserBryan <bkk@gmail.com>
2024-01-04 05:01:19 -0800

    Write a class Rectangle that defines a rectangle by: (based on 6-rectangle.py)

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
-Write a class Rectangle that defines a rectangle
+This is the "Rectangle"  module.
+
+This module provides a Rectangle class.
 """
 
 
 class Rectangle:
-    """creates class that defines a rectangle
+    """
+    creates class that defines a rectangle
     Attributes:
     empty
     """
@@ -24,7 +27,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -36,26 +39,35 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    def area(self):
-        return self.width * self.height
-
-    def perimeter(self):
-        return 2 * (self.width + self.height)
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __str__(self):
-        if self.width == 0 or self.height == 0:
-            return ""
-        return (str(self.print_symbol) * self.width + "\n") * self.height
-
-    def __repr__(self):
-        return f"Rectangle({self.width}, {self.height})"
+        total = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                try:
+                    total += str(self.print_symbol)
+                except Exception:
+                    total += type(self).print_symbol
+            if i is not self.__height - 1:
+                total += "\n"
+        return total
 
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return (2 * self.__width) + (2 * self.__height)
