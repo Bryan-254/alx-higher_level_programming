e2762e9148f3c35b1c57dd3933cfa3e7055a1d29
UserBryan <bkk@gmail.com>
2024-01-05 03:42:54 -0800

    Low memory cost task

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Documentation for a class LockedClass with no class or object attribute
+"""
+
+class LockedClass:
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        pass
