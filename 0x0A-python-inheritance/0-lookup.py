7c376b9757024dd9de5e0431dec12b8f459d2604
UserBryan <bkk@gmail.com>
2024-01-08 03:08:32 -0800

    Function that returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""The module for Lookup task"""
+
+
 def lookup(obj):
-    d = dir(obj)
-    return d
+    """
+    Returns all the atributes and methods the object obj has.
+    The dir() function is a built-in Python function that can
+    also be used to list all the attributes and methods of an object.
+    """
+
+    return (dir(obj))
