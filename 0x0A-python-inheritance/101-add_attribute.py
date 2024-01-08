e2369643e8a39d29f2dd6efb46edd6398aa223eb
UserBryan <bkk@gmail.com>
2024-01-08 13:27:31 -0800

    Function that adds a new attribute to an object if it’s possible

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,10 +6,14 @@ This involves defining function to check if you can add attribute
 
 def add_attribute(obj, name, value):
     """
-    checks if can add atrribute
+    checks if can add atrribute.
 
     The function checks if the object has a __dict__ attribute,
     which indicates that it allows adding new attributes.
+
+    The presence of __dict__ in an object or class often indicates
+    that you can dynamically add or modify attributes at runtime.
+
     If the object allows it, the function uses setattr to add
     the attribute with the specified name and value.
     """
