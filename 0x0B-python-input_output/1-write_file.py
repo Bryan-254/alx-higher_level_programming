323fae1cbc5840cb6bfff619a224a7edd46ac265
UserBryan <bkk@gmail.com>
2024-01-09 01:50:56 -0800

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,9 +10,13 @@ def write_file(filename="", text=""):
     file and automatically close it when the block of code
     inside the with statement is exited.
 
+    function directly returns the result of file.write(text),
+    which is the number of characters written.
+
     This code writes the specified text to the file using file.write(text)
-    and then prints the number of characters written directly without the
+    and then returns the number of characters written directly without the
     need for an intermediate variable.
     """
+
     with open(filename, 'w', encoding='utf-8') as file:
-        print(file.write(text), end="")
+        return file.write(text)
