3d67d9bab0c0205472853a7ba882d92dd069b8b3
UserBryan <bkk@gmail.com>
2024-01-15 08:58:36 -0800

    Dic to JSON & JSON to File tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,6 +10,7 @@ import turtle
 class Base:
     """
     Base class
+
     """
 
     __nb_objects = 0
@@ -20,3 +21,29 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries.
+        Use this method to convert lists of dictionaries to JSON strings.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of list_objs to a file
+        """
+        if list_objs is None:
+            list_objs = []
+
+        file_name = cls.__name__ + ".json"
+
+        with open(file_name, mode="w", encoding="utf-8") as file:
+            json_str = cls.to_json_string([obj.to_dictionary()
+                                           for obj in list_objs])
+            file.write(json_str)
