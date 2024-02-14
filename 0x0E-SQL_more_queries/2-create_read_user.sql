ffb01f445aeff5a9ab4120e376112b1d1ff15fad
UserBryan <bkk@gmail.com>
2024-02-14 03:51:20 -0800

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- Create database if it doesn't exist.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create user if it doesn't exist and set password.
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd'
 -- Grant SELECT privilege on hbtn_0d_2 database to user_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
