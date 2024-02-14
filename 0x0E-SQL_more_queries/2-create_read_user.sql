b6da554f69185d8400ea001dede7caeeaa1c0209
UserBryan <bkk@gmail.com>
2024-02-14 03:47:15 -0800

    Read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- SQL script that creates the database hbtn_0d_2 and the user user_0d_2.
+
+-- Create database if it doesn't exist.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user if it doesn't exist and set password.
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
+
+-- Grant SELECT privilege on hbtn_0d_2 database to user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
