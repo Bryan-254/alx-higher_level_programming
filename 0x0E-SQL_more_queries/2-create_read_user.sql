27eda050c2d61202aec313d5031ddb8acfa6fdbd
UserBryan <bkk@gmail.com>
2024-02-14 03:58:05 -0800

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,10 @@
--- creates the database hbtn_0d_2 and the user user_0d_2
--- creates a database
+-- SQL script that creates the database hbtn_0d_2 and the user user_0d_2.
+
+-- creates a database if it doesn't exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- creates a user
+
+-- creates a user if it doesn't exists
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
--- grants SELECT privileges to a user
+
+-- grants SELECT privileges on hbtn_0d_2 database to user_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
