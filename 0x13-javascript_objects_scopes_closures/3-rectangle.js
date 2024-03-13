9c4e2cd8ee68602ed3137c4602646b33e23c8cdd
UserBryan <bkk@gmail.com>
2024-03-13 05:55:11 -0700

    This implementation ensures that the Rectangle class is correctly defined with a constructor that checks for valid width and height values and an print() method that prints the rectangle using the character X.

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
+
+// Test cases
+const r1 = new Rectangle(2, 3);
+r1.print();
+
+const r2 = new Rectangle(10, 5);
+r2.print();
